f1626a215a15a69cd7781dea07a8025e4d2c8197
Oscar Perona <144930013+peronaOscar@users.noreply.github.com>
2023-12-12 23:10:33 +0100

    Bugs fixed

--- main.ts
+++ main.ts
@@ -491,97 +491,23 @@ scene.cameraFollowSprite(cursor)
 game.onUpdateInterval(1000, function () {
     if (gemerated_enemys_in_level < enemy_count && playerInShop == false) {
         newEnemy2 = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . f f f . . 
+            f f f . . . . . . . . c c f f f 
+            c b b c f . . . c c . c c c f f 
+            . c b b b f f c c 3 c c 3 c f f 
+            . c c c b b f c b 3 c b 3 c f f 
+            . c c b c b f c b b b b b b c f 
+            . c b b c b b c b 1 b b b 1 c c 
+            . c b c c c b b b b b b b b b c 
+            . . c c c c c b b c 1 f f 1 b c 
+            . . . c f b b b b f 1 f f 1 f c 
+            . . . c f b b b b f f f f f f f 
+            . . c c f b b b b f 2 2 2 2 f f 
+            . . . . f c b b b 2 2 2 2 2 f . 
+            . . . . . f c b b b 2 2 2 f . . 
+            . . . . . . f f f f f f f . . . 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Enemy)
-        animation.runImageAnimation(
-        newEnemy2,
-        [img`
-            . . . . b b b b . . . . . . . . 
-            . . . b 3 3 3 3 b b b b . . . . 
-            . . b b 3 3 3 3 3 1 1 b b c c . 
-            . . b 1 1 3 3 3 3 3 1 1 3 b c c 
-            . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
-            . . c 3 3 3 3 3 b c c c c b b f 
-            . c 3 3 3 3 b b d d d c c c b f 
-            c b 3 3 b b d d d d d d b c b f 
-            c 3 3 c b d d d d d d c d b c . 
-            f 3 c c c d d c d d d c d d c . 
-            f b c c c d d d c d d d d d f . 
-            f b c c c f f b d d b b b d f . 
-            f f b b f b d d b d d d d c . . 
-            . f f f f d d b b d d d c . . . 
-            . . . . b b b b f b b f f . . . 
-            . . . . . . . f f b b b f . . . 
-            `,img`
-            . . . . . . . . . . . . . . . . 
-            . . . . b b b b . . . . . . . . 
-            . . . b 3 3 3 3 b b b b . . . . 
-            . . b b 3 3 3 3 3 1 1 b b c c . 
-            . . b 1 1 3 3 3 3 1 1 1 3 c c c 
-            . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
-            . . c 1 1 3 3 b b c c c c b b f 
-            . c c 3 3 b b d d d d b c c b f 
-            c b 3 3 b b d d d d d d d c b f 
-            c 3 3 b b d d d d d d c d d c . 
-            f 3 3 c b d d c d d d c d d c . 
-            f b c c c d d d c d d d d d f . 
-            f b c c c d d f f b b b b d f . 
-            f f b b c c f b d d b d d c . . 
-            . f f f c c f d d b b d c . . . 
-            . . . . . . b b b b f c . . . . 
-            `,img`
-            . . . . . . . . . . . . . . . . 
-            . . . . b b b b . . . . . . . . 
-            . . . b 3 3 3 3 b b b b . . . . 
-            . . b b 3 3 3 3 3 3 1 1 b c c . 
-            . . b 3 3 3 3 3 3 1 1 1 3 c c c 
-            . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
-            . . c 1 1 3 3 3 b c c c c b b f 
-            . c c 3 3 3 b b d d d c c c b f 
-            c b 3 3 b b d d d d d d b c b f 
-            c 3 3 c b d d d d d d d d b c . 
-            f 3 c c c d d d d d d c c d c . 
-            f b c c c d d c c d d d d d f . 
-            f b c c c d d d d d b b b d f . 
-            f f b b c f f b d d d d d c . . 
-            . f f f f d d b b d d d b f . . 
-            . . . . f d d d b c c f f f . . 
-            `,img`
-            . . . . b b b b . . . . . . . . 
-            . . . b 3 3 3 3 b b b b . . . . 
-            . . b b 3 3 3 3 3 1 1 b b c c . 
-            . . b 1 1 3 3 3 3 3 1 1 3 3 c c 
-            . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
-            . . c 3 3 3 3 3 3 3 c c c b b f 
-            . c 3 3 3 3 3 b b b b c c c b f 
-            c 3 3 3 3 b b d d d d d c c b f 
-            c 3 3 c b d d d d d d c d c c . 
-            f 3 c c c d d c d d d c d b c . 
-            f b c c c d d d c d d d d d f . 
-            f b c c c d d d d d b b b d f . 
-            f f b b c b d d d d d d d c . . 
-            . f f f f b c c d d d d f f . . 
-            . . f b d d b c c f f b b f f . 
-            . . f d d d b . . f f b b b f . 
-            `],
-        100,
-        true
-        )
         tiles.placeOnRandomTile(newEnemy2, assets.tile`myTile0`)
         newEnemy2.vy = enemySpeed
         generated_enemys += 1
