bb007580156fa965c1c98c26a7982082a9dbc2bf
Oscar Perona <144930013+peronaOscar@users.noreply.github.com>
2023-12-12 17:22:48 +0100

    animation

--- main.ts
+++ main.ts
@@ -410,6 +410,60 @@ game.onUpdateInterval(1000, function () {
     if (gemerated_enemys_in_level < enemy_count && playerInShop == false) {
         newEnemy2 = sprites.create(img`
             . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        animation.runImageAnimation(
+        newEnemy2,
+        [img`
+            . . . . b b b b . . . . . . . . 
+            . . . b 3 3 3 3 b b b b . . . . 
+            . . b b 3 3 3 3 3 1 1 b b c c . 
+            . . b 1 1 3 3 3 3 3 1 1 3 b c c 
+            . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+            . . c 3 3 3 3 3 b c c c c b b f 
+            . c 3 3 3 3 b b d d d c c c b f 
+            c b 3 3 b b d d d d d d b c b f 
+            c 3 3 c b d d d d d d c d b c . 
+            f 3 c c c d d c d d d c d d c . 
+            f b c c c d d d c d d d d d f . 
+            f b c c c f f b d d b b b d f . 
+            f f b b f b d d b d d d d c . . 
+            . f f f f d d b b d d d c . . . 
+            . . . . b b b b f b b f f . . . 
+            . . . . . . . f f b b b f . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . b b b b . . . . . . . . 
+            . . . b 3 3 3 3 b b b b . . . . 
+            . . b b 3 3 3 3 3 1 1 b b c c . 
+            . . b 1 1 3 3 3 3 1 1 1 3 c c c 
+            . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+            . . c 1 1 3 3 b b c c c c b b f 
+            . c c 3 3 b b d d d d b c c b f 
+            c b 3 3 b b d d d d d d d c b f 
+            c 3 3 b b d d d d d d c d d c . 
+            f 3 3 c b d d c d d d c d d c . 
+            f b c c c d d d c d d d d d f . 
+            f b c c c d d f f b b b b d f . 
+            f f b b c c f b d d b d d c . . 
+            . f f f c c f d d b b d c . . . 
+            . . . . . . b b b b f c . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
             . . . . b b b b . . . . . . . . 
             . . . b 3 3 3 3 b b b b . . . . 
             . . b b 3 3 3 3 3 3 1 1 b c c . 
@@ -425,7 +479,27 @@ game.onUpdateInterval(1000, function () {
             f f b b c f f b d d d d d c . . 
             . f f f f d d b b d d d b f . . 
             . . . . f d d d b c c f f f . . 
-            `, SpriteKind.Enemy)
+            `,img`
+            . . . . b b b b . . . . . . . . 
+            . . . b 3 3 3 3 b b b b . . . . 
+            . . b b 3 3 3 3 3 1 1 b b c c . 
+            . . b 1 1 3 3 3 3 3 1 1 3 3 c c 
+            . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+            . . c 3 3 3 3 3 3 3 c c c b b f 
+            . c 3 3 3 3 3 b b b b c c c b f 
+            c 3 3 3 3 b b d d d d d c c b f 
+            c 3 3 c b d d d d d d c d c c . 
+            f 3 c c c d d c d d d c d b c . 
+            f b c c c d d d c d d d d d f . 
+            f b c c c d d d d d b b b d f . 
+            f f b b c b d d d d d d d c . . 
+            . f f f f b c c d d d d f f . . 
+            . . f b d d b c c f f b b f f . 
+            . . f d d d b . . f f b b b f . 
+            `],
+        500,
+        true
+        )
         tiles.placeOnRandomTile(newEnemy2, assets.tile`myTile0`)
         newEnemy2.vy = enemySpeed
         generated_enemys += 1
